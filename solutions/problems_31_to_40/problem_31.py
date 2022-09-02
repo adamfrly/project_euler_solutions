e6a290c7a5c42aa3c27350baf67c73a108e49fe3
Adam Farley <adamfrly@gmail.com>
2022-09-02 18:56:02 -0400

    Add problem 31

---
+++ solutions/problems_31_to_40/problem_31.py
@@ -0,0 +1,15 @@
+def count_change(coins, n, sum):
+    if (sum == 0):
+        return 1
+    if (sum < 0):
+        return 0
+    if (n <= 0):
+        return 0
+ 
+    return count_change(coins, n - 1, sum) + count_change(coins, n, sum-coins[n-1])
+
+coin_purse = [1, 2, 5, 10, 20, 50, 100, 200]
+n = len(coin_purse)
+total = 200
+
+print(count_change(coin_purse, n, total))
